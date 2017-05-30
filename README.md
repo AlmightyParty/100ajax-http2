0db029937f442cbea32f0b825d34d80b435e1ab0
yangchongduo <yangchongduo@qudian.com>
2017-05-30 08:47:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HTTP2-NodeJS-Demo
+HTTP2 Node.js Demo
+
+## run http2 server
+
+```js
+node http2test.js
+```
+
+## 
---
+++ http2test.js
@@ -0,0 +1,33 @@
+/**
+ * @author: Jason.占友伟 zhanyouwei@icloud.com
+ * Created on 16/3/9.
+ */
+var http2 = require('http2');
+var fs = require('fs');
+
+var options = {
+	key: fs.readFileSync(__dirname + '/tsl/server.pass.key'),
+	cert: fs.readFileSync(__dirname + '/tsl/server.crt'),
+	NPNProtocols: ['h2', 'http 1.1', 'http 1.0'],
+	passphrase: '1234'
+};
+console.log('dddddddd')
+http2.createServer(options, function (request, response) {
+	var url = request.url;
+	console.log('--------------');
+	//跨域处理
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+	response.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+	response.setHeader("X-Powered-By", ' 3.2.1');
+	if (url === '/') {
+		console.log('http2');
+		response.end('Hello! This is HTTP2 Server!');
+	} else if (url === '/http2test') {
+		setTimeout(function () {
+			response.end('http2 success');
+		}, 300);
+	}
+}).listen(8080,()=>{
+	console.log('server created port:8080  / || http2test');
+});
